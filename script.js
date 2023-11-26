01ddf43de28bad12c088e0c8ed2a7b459a18a639
Krisanath581 <krisanath581@gmail.com>
2023-11-27 00:09:18 +0700

    submit , Password matching ,error msg

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function myFunction() {
-    alert("Saved !");
-  }
+function matchPassword() {
+    let password = document.getElementById("password").value;
+    let confirmpassword = document.getElementById("confirmpassword").value;
+    let message = document.getElementById('showError');
+   // console.log(password,confirmpassword);
+      if(password.length != 0){
+        if(password == confirmpassword){
+          alert("Saved !");
+        }else{
+          message.textContent = "Password don't match";
+          event.preventDefault(); // stop refreshing the page on submit
+        }
+      }      
+    }
+  
